1648d0bf425bd22394563670e0bc6c536003dd61
Neno Almeida <nenoxvx@icloud.com>
2016-09-29 19:00:24 -0300

    Rodando o instalador

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929200751) do
+ActiveRecord::Schema.define(version: 20160929215931) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
